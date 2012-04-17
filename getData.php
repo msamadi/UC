26bd466e99a99f5cbe9e9ec24e5e22138ade3c12
Seena Abdollahi <sa.shirazu@gmail.com>
2012-04-17 18:44:22 +0430

    Read the Data from XML file

---
+++ getData.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: zx
+ * Date: 4/17/12
+ * Time: 2:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ js/functions.js
@@ -0,0 +1,7 @@
+/**
+ * Created by JetBrains PhpStorm.
+ * User: zx
+ * Date: 4/16/12
+ * Time: 4:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
